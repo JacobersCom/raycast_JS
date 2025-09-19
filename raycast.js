e0797006d1d401e3cd488d8e3347f581e58052ad
JacobersCom <Jacobymartin1@icloud.com>
2025-09-18 23:35:57 -0400

    Distance function fixed. Rendering walls works

--- raycast.js
+++ raycast.js
@@ -257,7 +257,7 @@ var rays = [];
 
 function distanceBetweenPoints(x1,y1,x2,y2){
 
-    return Math.sqrt((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1));
+    return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
 function normalAngle(angle){
